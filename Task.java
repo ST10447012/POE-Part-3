5bf1bd0ae6dc30097021e616922948b1c01373c7
ST10447012 <sanelisohlophe27@gmail.com>
2025-01-16 11:47:58 +0200

    Update Task.java

--- Task.java
+++ Task.java
@@ -2,46 +2,119 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package taskpoe;
+package com.mycompany.taskmanager;
+
+import java.util.ArrayList;
+import java.util.regex.*;
+import javax.swing.*;
 
 /**
  *
- * @author RC_Student_lab
+ * @author lab_services_student
  */
 public class Task {
-    private String developer;
     private String taskName;
+    private int taskNumber;
+    private String taskDescription;
+    private String developerDetails;
     private int taskDuration;
+    private String taskID;
     private String taskStatus;
 
-    public Task(String developer, String taskName, int taskDuration, String taskStatus) {
-        this.developer = developer;
+    // Constructor to initialize a Task instance
+    public Task(String taskName, int taskNumber, String taskDescription, String developerDetails, int taskDuration, String taskStatus) {
         this.taskName = taskName;
+        this.taskNumber = taskNumber;
+        this.taskDescription = taskDescription;
+        this.developerDetails = developerDetails;
         this.taskDuration = taskDuration;
         this.taskStatus = taskStatus;
+        this.taskID = generateTaskID(); // Generate task ID automatically
+         // Generate task ID
+       
     }
 
-    public String getDeveloper() {
-        return developer;
+    /**
+     * Generates a unique Task ID based on the task name, task number, and
+     * developer details.
+     * 
+     * @return A string representing the unique task ID.
+     */
+    private String generateTaskID() {
+        return taskName.substring(0, 2).toUpperCase() + ":" + taskNumber + ":" + 
+               developerDetails.substring(developerDetails.length() - 3).toUpperCase();
     }
 
+    // Getters and Setters
     public String getTaskName() {
         return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public int getTaskNumber() {
+        return taskNumber;
+    }
+
+    public void setTaskNumber(int taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public String getDeveloperDetails() {
+        return developerDetails;
+    }
+
+    public void setDeveloperDetails(String developerDetails) {
+        this.developerDetails = developerDetails;
     }
 
     public int getTaskDuration() {
         return taskDuration;
     }
 
+    public void setTaskDuration(int taskDuration) {
+        this.taskDuration = taskDuration;
+    }
+
+    public String getTaskID() {
+        return taskID;
+    }
+
+    public void setTaskID(String taskID) {
+        this.taskID = taskID;
+    }
+
     public String getTaskStatus() {
         return taskStatus;
     }
 
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    /**
+     * Returns a string representation of the task, formatted for easy reading.
+     * 
+     * @return A string containing task details.
+     */
     @Override
     public String toString() {
-        return "Developer: " + developer +
-               ", Task Name: " + taskName +
-               ", Duration: " + taskDuration + " hours" +
-               ", Status: " + taskStatus;
+        return "Task Name: " + taskName +
+               "\nTask Number: " + taskNumber +
+               "\nTask Description: " + taskDescription +
+               "\nDeveloper Details: " + developerDetails +
+               "\nTask Duration: " + taskDuration + " hours" +
+               "\nTask ID: " + taskID +
+               "\nTask Status: " + taskStatus;
     }
 }
